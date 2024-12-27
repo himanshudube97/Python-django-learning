42bf8770b5acfc9f282d270b1498270623517542
himanshudube97 <himanshu.dube13@gmail.com>
2024-12-27 14:24:14 +0530

    removed

--- FirstLearning/drinks/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Drink(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=500)
-
-    def __str__(self):  # this functions gives the name to the entries in the admin ui.
-        return self.name
