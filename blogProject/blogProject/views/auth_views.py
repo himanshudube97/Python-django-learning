44df86cc6bf7ad47442eb36c1f6f84c702a18b73
himanshudube97 <himanshu.dube13@gmail.com>
2025-01-01 20:54:43 +0530

    fixes

--- blogProject/blogProject/views/auth_views.py
+++ blogProject/blogProject/views/auth_views.py
@@ -19,33 +19,31 @@ class Auth(ViewSet):
     # ## Writing the functions here. permission_classes = [AllowAny]
     @api_view(['POST'])
     def createUser(self, request):
-        print(request, "request")
-        return Response({"data":{},    'message': 'User created successfully'}, status=status.HTTP_200_OK)
             ## sending to serielizer
         serializer = UserSerializer(data=request.data)
-    #     if(serializer.is_valid()):
-    #         ## extracting valid data
-    #         email = serializer.validated_data.get('email')
-    #         password = serializer.validated_data.get('password')
-    #         confirm_password = serializer.validated_data.get('confirm_password')
+        if(serializer.is_valid()):
+            ## extracting valid data
+            email = serializer.validated_data.get('email')
+            password = serializer.validated_data.get('password')
+            confirm_password = serializer.validated_data.get('confirm_password')
 
-    #         ## checking if password and confirm password are same
-    #         if(password != confirm_password):
-    #             return Response({"data":{},    'message': 'Password and Confirm Password do not match'}, status=status.HTTP_400_BAD_REQUEST)
+            ## checking if password and confirm password are same
+            if(password != confirm_password):
+                return Response({"data":{},    'message': 'Password and Confirm Password do not match'}, status=status.HTTP_400_BAD_REQUEST)
             
-    #         ##checking if user already exists
-    #         user = User.objects.filter(email=email).first()
+            ##checking if user already exists
+            user = User.objects.filter(email=email).first()
 
-    #         if(user):
-    #             return Response({"data":{},    'message': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
+            if(user):
+                return Response({"data":{},    'message': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
             
-    #         ## creating user
-    #         user = User.objects.create_user(email=email, password=password)
-    #         user.save()
-    #         return Response({"data":user,    'message': 'User created successfully'}, status=status.HTTP_200_OK)
+            ## creating user
+            user = User.objects.create_user(email=email, password=password)
+            user.save()
+            return Response({"data":user,    'message': 'User created successfully'}, status=status.HTTP_200_OK)
 
-    # ## when serielzier is not valid
-    #     return Response({"data":{},    'message': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+    ## when serielzier is not valid
+        return Response({"data":{},    'message': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
 
         
 
